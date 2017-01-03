004b204165360f822fcd2e7a78281e1935048c8d
xionglihui <xionglihui@vip.qq.com>
2017-01-03 14:22:15 +0800

    android studio

--- app/src/main/java/com/xiong/dandan/wudd/ui/MainActivity.java
+++ app/src/main/java/com/xiong/dandan/wudd/ui/MainActivity.java
@@ -15,7 +15,5 @@ public class MainActivity  extends BaseActivity  {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 }
